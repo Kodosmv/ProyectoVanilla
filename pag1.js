3db9e2aa7e971c551e64a0b145d05b8e00b7452c
Kodosmv <soledat10@gmail.com>
2025-11-13 18:55:22 +0100

    13

--- pag1.js
+++ pag1.js
@@ -1,53 +1,30 @@
-
-function procesarPago(producto) {
-  	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			if(producto.trim() !== "") {
-				resolve(`Pago procesado correctamente para ${producto}`);
-			}else {
-				reject("No se puede procesar el pago sin producto");
-			}
-		}, 1500);
-	});
-}
-
-
-function enviarPedido(direccion){
-	return new Promise((resolve, reject) => {
-		if (direccion.trim() === ""){
-			reject("Dirección de envío no válida");
-			return;
-		}
-
-		setTimeout(() => {
-			let randomError = Math.random() < 0.2; 
-			if (randomError){
-				reject("Error durante el envío, intente de nuevo.");
-			}else{
-				resolve(`Pedido enviado correctamente a ${direccion}`);
-			}
-		}, 2000);
-	});
-}
-
-
-document.getElementById("boton").addEventListener("click", (event) => {
-  	event.preventDefault();
-	let producto = document.getElementById("producto").value;
-  	let envio = document.getElementById("envio").value;
-  	let resul = document.getElementById("resul");
-
- 	resul.textContent = ".......Espere......";
-
-	procesarPago(producto)
-		.then((pago) => {
-			resul.textContent = pago;
-			return enviarPedido(envio);
-		})
-		.then((envio) => {
-			resul.textContent = envio;
-		})
-		.catch((error) => {
-			resul.textContent = error;
-		});
+document.addEventListener('keydown', function (event) {
+    if (event.ctrlKey && event.key === 'y') {
+        event.preventDefault();
+        cambiarVista();
+    }
 });
+
+
+function cambiarVista() {
+    const vistaDiv = document.getElementById('vista');
+    vistaDiv.textContent= "";
+    const label = document.createElement('label');
+    label.id = 'usu';
+    label.textContent= 'Usuario';
+    const br = document.createElement('br');
+    const input = document.createElement('input');
+    input.type='email';
+    input.id='usu';
+   
+
+
+    vistaDiv.appendChild(label);
+    vistaDiv.appendChild(br);
+    vistaDiv.appendChild(input);
+
+  
+}
+setTimeout(() => {
+    cambiarVista();
+}, 5000);
